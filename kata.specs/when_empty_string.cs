a1e2508c971b4262bc5304d7c3ad2f782c289d26
Dante Castillo Dubón <dante.dubon@gmail.com>
2015-05-22 13:42:30 -0600

    first test, empty string failing

---
+++ kata.specs/when_empty_string.cs
@@ -0,0 +1,21 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_empty_string
+    {
+        static StringCalculator calculator;
+        static int result;
+        Establish context = () =>
+        {
+            calculator = new StringCalculator();
+        };
+
+        Because of = () =>
+        {
+            result = calculator.add("");
+        };
+
+        It should_return_zero = () => { result.ShouldEqual(0); };
+    }
+}
---
+++ kata/StringCalculator.cs
@@ -0,0 +1,10 @@
+namespace kata
+{
+    public class StringCalculator
+    {
+        public int add(string empty)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
