61dcd119731503955928e845c5af56dc2671055c
Dante Castillo Dubón <dante.dubon@gmail.com>
2015-05-22 14:38:11 -0600

    unknown numbers, failing

--- kata.specs/when_two_number.cs
+++ kata.specs/when_two_number.cs
@@ -6,22 +6,21 @@ using Machine.Specifications;
 
 namespace kata.specs
 {
-    public class when_unknown_numbers
+    public class when_two_number
     {
         static StringCalculator calculator;
         static int result;
         Establish context = () =>
         {
             calculator = new StringCalculator();
-            
         };
 
         Because of = () =>
         {
-            result = calculator.add("1,2,3");
+            result = calculator.add("1,2");
         };
 
-        It should_return_sum = () => { result.ShouldEqual(6); };
+        It should_return_sum = () => { result.ShouldEqual(3); };
     }
 
 }
