a6d14e1a9d996a2a6ea336fc442f3986b591fc18
Dante Castillo Dubón <dante.dubon@gmail.com>
2015-05-22 14:14:06 -0600

    test single number failing

---
+++ kata.specs/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_single_number
+    {
+        static StringCalculator calculator;
+        static int result;
+        Establish context = () =>
+        {
+            calculator = new StringCalculator();
+            
+        };
+
+        Because of = () =>
+        {
+            result = calculator.add("1");
+        };
+
+        It should_return_same_number = () => { result.ShouldEqual(1); };
+    }
+
+}
