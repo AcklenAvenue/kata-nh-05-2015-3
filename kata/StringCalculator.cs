c376770e2ba2137cfe29a78ce8636695a7ce2819
Osman Mejia <osmaned@gmail.com>
2015-05-22 14:35:13 -0600

    Third Test

--- kata/StringCalculator.cs
+++ kata/StringCalculator.cs
@@ -6,10 +6,17 @@ namespace kata
     {
         public int add(string empty)
         {
+            char[] delimeter = {','};
+            
             if (empty.Equals(""))
                 return 0;
-            
-            return Convert.ToInt32(empty); 
+            else if (empty.Length==1)
+                return Convert.ToInt32(empty); 
+
+            string[] numbers = empty.Split(delimeter);
+
+            return Convert.ToInt32(numbers[0]) + Convert.ToInt32(numbers[1]);
+
         }
     }
 }
